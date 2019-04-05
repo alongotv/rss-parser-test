cf43783a239d949103ae770a3a7aef008efbf1aa
alongotv <pdastore1@gmail.com>
2019-04-05 18:59:16 +0300

    add feed view controller

---
+++ Project/Controller/FeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedViewController.swift
+//  rss-parser-test
+//
+//  Created by Vladimir Vetrov on 05/04/2019.
+//  Copyright © 2019 Vladimir Vetrov. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
