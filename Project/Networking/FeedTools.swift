822bab2d45a4a8817627dd8be037e7f623db6cff
alongotv <pdastore1@gmail.com>
2019-05-22 01:42:17 +0300

    Add FeedTools class

---
+++ Project/Networking/FeedTools.swift
@@ -0,0 +1,24 @@
+//
+//  FeedTools.swift
+//  rss-parser-test
+//
+//  Created by Vladimir Vetrov on 01/05/2019.
+//  Copyright © 2019 Vladimir Vetrov. All rights reserved.
+//
+
+import Foundation
+import FeedKit
+
+class FeedTools {
+    func fetchNewsItemsAsync(from URL: URL, completionHandler: @escaping (Result) -> ()){
+        let parser = FeedParser.init(URL: URL)
+        parser.parseAsync(queue: DispatchQueue.global(qos: .userInitiated),
+        result: {
+            result in
+            DispatchQueue.main.async {
+                completionHandler(result)
+            }
+        })
+
+    }
+}
