8d1b6d333389bab8f2a5a30a9a517850c49416ff
alongotv <pdastore1@gmail.com>
2019-05-22 01:44:20 +0300

    Add SectionHeader class for supplementary view

---
+++ Project/View/SectionHeader.swift
@@ -0,0 +1,15 @@
+//
+//  HeaderCollectionReusableView.swift
+//  rss-parser-test
+//
+//  Created by Vladimir Vetrov on 14/04/2019.
+//  Copyright © 2019 Vladimir Vetrov. All rights reserved.
+//
+
+import UIKit
+
+class SectionHeader: UICollectionReusableView {
+    
+    @IBOutlet weak var sectionHeaderLabel: UILabel!
+    
+}
