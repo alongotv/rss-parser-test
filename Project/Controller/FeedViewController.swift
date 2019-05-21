c7ad230a7ceb4274678053a4a5b42c7cf0b823e6
alongotv <pdastore1@gmail.com>
2019-05-22 01:45:40 +0300

    Change view controller type to collection view controller

--- Project/Controller/FeedViewController.swift
+++ Project/Controller/FeedViewController.swift
@@ -7,24 +7,75 @@
 //
 
 import UIKit
+import CoreData
+import FeedKit
 
-class FeedViewController: UIViewController {
-
+class FeedViewController: UICollectionViewController {
+    
+    var sources = [RssSource]()
+    var newsItems = [RSSFeedItem]()
+    let coreDataRepository = Persistence()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        sources = coreDataRepository.fetchSourcesFromCoreData()
+        
+        let parser = FeedParser.init(URL: URL(string: "https://images.apple.com/main/rss/hotnews/hotnews.rss")!)
+        let result = parser.parse()
+        newsItems = result.rssFeed!.items!
+        print("NewsItems = \(newsItems.count)")
+        
+        collectionView.reloadData()
+        
+        for source in newsItems {
+            print(source.title)
+        }
         // Do any additional setup after loading the view.
     }
     
-
-    /*
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return sources.count
+    }
+    
+    //    func numberOfItems(inSection section: Int) -> Int {
+    //        return newsItems.count
+    //    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return newsItems.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        if let sectionHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "SectionHeader", for: indexPath) as? SectionHeader{
+            sectionHeader.sectionHeaderLabel.text = "Section \(indexPath.row)"
+            return sectionHeader
+        }
+        return UICollectionReusableView()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RssCollectionViewCellIdentifier", for: indexPath) as! RssCollectionViewCell
+        
+        cell.cellTitle.text = newsItems[indexPath.item].title
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "newsItemCellToNewsItemViewController", sender: sources[indexPath.item])
+    }
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-    }
-    */
+        if segue.identifier == "newsItemCellToNewsItemViewController" {
+            let controller = (segue.destination as! UINavigationController).viewControllers[0] as! NewsItemViewController
 
+        }
+    }
 }
