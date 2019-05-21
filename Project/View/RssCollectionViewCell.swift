1a2aaf847a38792f25af84e33e28074808dd6e4f
alongotv <pdastore1@gmail.com>
2019-05-22 01:44:42 +0300

    Add Rss collection view cell class

---
+++ Project/View/RssCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  RssCollectionViewCell.swift
+//  rss-parser-test
+//
+//  Created by Vladimir Vetrov on 19/04/2019.
+//  Copyright © 2019 Vladimir Vetrov. All rights reserved.
+//
+
+import UIKit
+
+class RssCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellTitle: UILabel!
+    
+}
