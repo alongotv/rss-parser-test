92b53b79308106d1633ceda4d4c93e9a361fd0f6
alongotv <pdastore1@gmail.com>
2019-05-24 20:51:54 +0300

    Fix code formatting

--- Project/Controller/NewsItemViewController.swift
+++ Project/Controller/NewsItemViewController.swift
@@ -23,6 +23,7 @@ class NewsItemViewController: UIViewController {
 
         textItemLabel.text = newsItem.title
         textItemDescription.text = newsItem.description        // Do any additional setup after loading the view.
+        
     }
     
 
