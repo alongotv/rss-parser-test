80f57fa58c57a89e935ee9515d2e60bf307eee3d
alongotv <pdastore1@gmail.com>
2019-06-03 14:49:20 +0300

    Add CoreDataInstanceDelegate protocol

---
+++ Project/View/CoreDataInstanceDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataInstanceDelegate.swift
+//  rss-parser-test
+//
+//  Created by Vladimir on 03/06/2019.
+//  Copyright © 2019 Vladimir Vetrov. All rights reserved.
+//
+
+import Foundation
+
+protocol CoreDataInstanceDelegate {
+    func coreDataContentsDidChange()
+}
